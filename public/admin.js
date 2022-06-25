9b71d1a22cfefb5fb8d314bda256a587a6b848c1
Balthier <rrodrijose@gmail.com>
2022-06-24 22:55:56 -0500

    li element

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,18 @@
 
 // Your Code Here
+async function admin(){
+const bookListResponse  = await fetch ('http://localhost:3001/listBooks');
+const bookList = await bookListResponse.json();
+
+
+
+
+bookList.forEach(book => {
+    const title= book.title;
+
+    const li = document.createElement('li')
+    bookList.textcontent= book.title;
+    
+});
+}
+admin()
